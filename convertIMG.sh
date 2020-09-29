5665a9c1381dbc76b011bc757ac2d911d271b758
Kaho-Lee <ljh243771593@hotmail.com>
2020-09-29 19:00:49 +0100

    cleaning

--- convertIMG.sh
+++ convertIMG.sh
@@ -1,5 +1,7 @@
+#https://stackoverflow.com/questions/32946436/read-img-medical-image-without-header-in-python/32950492
+
 for i in {100..160}
 do
     echo "output: $i"
-    magick convert -size 2048x2048 -depth 16 -endian MSB -normalize gray:../JSRT/All247images/JPCLN$i.IMG ../JSRT/All247/JPCLN$i.png
+    convert -size 2048x2048 -depth 16 -endian MSB -normalize gray:../JSRT/All247images/JPCLN$i.IMG ../JSRT/All247/JPCLN$i.png
 done
